552b9a683edbfcf2e6654711961185851edd834e
kaitlynharsch <85089285+kaitlynharsch@users.noreply.github.com>
2021-12-03 08:59:01 -0700

    slideshow

--- Final/athletics.js
+++ Final/athletics.js
@@ -1,1 +1,28 @@
-<button onclick="window.location.href='https://cubuffs.com/sports/skiing/roster/kaitlyn-harsch/15709'">Continue</button>
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" picactive", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " picactive";
+}
